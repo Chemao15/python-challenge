68ff573bf8bbdfee90476d2d39ac9de6fa41bc85
Chemao15 <chema_1889@hotmail.com>
2023-04-18 14:04:53 -0400

    Write Data

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -1,5 +1,5 @@
 import os, csv
-csvpath = os.path.join('PyBank','Resources','budget_data.csv')
+csvpath = os.path.join('budget_data.csv')
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -9,12 +9,16 @@ with open(csvpath) as csvfile:
     contador_maximo = 0
     maximo = 0
     menor = 0
+    valor_maximo = 0
     m=[]
     n=[]
+    y=[]
     for row in csvreader:
         x=int(row[1])
+        dia=row[0]
         contador_pro = contador_pro + x
         m.append(x)
+        y.append(dia)
         contador_mes = contador_mes + 1
 
         if contador_mes > 1:
@@ -22,14 +26,34 @@ with open(csvpath) as csvfile:
             contador_maximo = contador_maximo + maximo
             n.append(maximo)
         
-        if contador_mes > 2:
-            n[contador_mes]>n[contador_mes-1]
-
-
-    print ('Financial Analysis')
-    print (f'Total Months: {contador_mes}')
-    print (f'Total: {contador_pro}')
-    print (f'Average Change: {contador_maximo/len(n)}')
-    print (f'Greatest Increase in Profits: {max (n)}')
-    print (f'Greatest Decrease in Profits: {min (n)}')
+        if contador_mes == 3:
+            segundo=n[contador_mes-2]
+            primero=n[contador_mes-3]
+            if segundo>primero:
+                valor_maximo=segundo
+                valor_minimo=primero
+                dia_mes = (y[contador_mes-1])
+                dia_mes_m = (y[contador_mes-1])
+            else:
+                valor_maximo=primero
+                dia_mes = (y[contador_mes-1])
+                valor_minimo=segundo
+                dia_mes_m = (y[contador_mes-1])
+        if contador_mes > 3:
+            if n[contador_mes-2]>valor_maximo:
+                valor_maximo= n[contador_mes-2]
+                dia_mes = (y[contador_mes-1])
+            if n[contador_mes-2]<valor_minimo:
+                valor_minimo= n[contador_mes-2]
+                dia_mes_m = (y[contador_mes-1])
     
+archivo = os.path.join('analysis.txt')      
+with open (archivo, 'w') as file_object:
+    file_object.write('Financial Analysis \n')
+    file_object.write('---------------------------- \n')
+    file_object.write(f'Total Months: {contador_mes}\n')
+    file_object.write(f'Total: {contador_pro}\n')
+    file_object.write(f'Average Change: {contador_maximo/len(n):.2f}\n')
+    file_object.write(f'Greatest Increase in Profits: {dia_mes} ({valor_maximo})\n')
+    file_object.write(f'Greatest Decrease in Profits: {dia_mes_m} ({valor_minimo})\n')
+     
--- PyPoll/Resources/main.py
+++ PyPoll/Resources/main.py
@@ -1,12 +1,12 @@
 import os, csv
-csvpath = os.path.join('PyPoll','Resources','election_data.csv')
+csvpath = os.path.join('Resources','election_data.csv')
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     
     
     csv_header= next (csvreader)
-    print(f'CSV Header {csv_header}')
+   
 
     contador_votos=0
     contador_charles=0
@@ -24,20 +24,20 @@ with open(csvpath) as csvfile:
         
         contador_votos = contador_votos + 1
 
-    
-    
-print('Election Results')
-print('-------------------------')
-print(f'Total Votes: {contador_votos}')
-print('-------------------------')
-print(f'Charles Casper Stockham: {(contador_charles/contador_votos)*100.:.2f}% ({contador_charles})')
-print(f'Diana DeGette: {(contador_diana/contador_votos)*100.:.2f}% ({contador_diana})')
-print(f'Raymon Anthony Doane: {(contador_raymo/contador_votos)*100.:.2f}% ({contador_raymo})')
-print('-------------------------')
-if contador_charles>contador_diana and contador_charles>contador_raymo:
-    print ('Winner: Charles Casper Stockham')
-if contador_diana>contador_charles and contador_diana>contador_raymo:
-    print('Winner: Diana DeGette')
-if contador_raymo>contador_charles and contador_raymo>contador_diana:
-    print('Raymon Anthony Doane')
-print('-------------------------')
+archivo = os.path.join('Resources','analysis.txt')     
+with open (archivo, 'w') as file_object:
+    file_object.write('Election Results\n')
+    file_object.write('-------------------------\n')
+    file_object.write(f'Total Votes: {contador_votos}\n')
+    file_object.write('-------------------------\n')
+    file_object.write(f'Charles Casper Stockham: {(contador_charles/contador_votos)*100.:.2f}% ({contador_charles})\n')
+    file_object.write(f'Diana DeGette: {(contador_diana/contador_votos)*100.:.2f}% ({contador_diana})\n')
+    file_object.write(f'Raymon Anthony Doane: {(contador_raymo/contador_votos)*100.:.2f}% ({contador_raymo})\n')
+    file_object.write('-------------------------\n')
+    if contador_charles>contador_diana and contador_charles>contador_raymo:
+       file_object.write('Winner: Charles Casper Stockham\n')
+    if contador_diana>contador_charles and contador_diana>contador_raymo:
+       file_object.write('Winner: Diana DeGette\n')
+    if contador_raymo>contador_charles and contador_raymo>contador_diana:
+       file_object.write('Raymon Anthony Doane\n')
+    file_object.write('-------------------------\n')
